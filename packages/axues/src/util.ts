047b4c936bb1d30d564fa6a1f0a2c2408e6d7284
dongnaebi <zhouzixian@qq.com>
2022-12-30 01:10:22 +0800

    feat: magic CancelablePromise

--- packages/axues/src/util.ts
+++ packages/axues/src/util.ts
@@ -140,3 +140,43 @@ export function transformErrorOptions<TAction> (options: ErrorOverlayOptions<TAc
 export function getCacheKey<T> (cacheKey?: MaybeComputedOrActionRef<string>, actionPayload?: T) {
   return resolveComputedOrActionRef(cacheKey, actionPayload).value
 }
+
+// todo test it
+export class CancelablePromise<T> extends Promise<T> {
+  canceled = false
+  constructor (executor: (resolve: any, reject: any, cancel: any) => CancelablePromise<T>) {
+    super((resolve, reject) => {
+      executor(resolve, reject, () => {
+        setTimeout(() => {
+          this.canceled = true
+        })
+      })
+    })
+  }
+
+  then<TResult1 = T, TResult2 = never>(
+    onfulfilled?: ((value: T) => PromiseLike<TResult1> | TResult1) | undefined | null,
+    onrejected?: ((reason: any) => PromiseLike<TResult2> | TResult2) | undefined | null
+  ): Promise<TResult1 | TResult2> {
+    if (this.canceled) {
+      // @ts-expect-error t
+      return this
+    }
+    return super.then(onfulfilled, onrejected)
+  }
+
+  catch<TResult = never>(onrejected?: ((reason: any) => PromiseLike<TResult> | TResult) | undefined | null): Promise<T | TResult> {
+    if (this.canceled) {
+      return this
+    }
+    return super.then(onrejected)
+  }
+
+  finally (onfinally?: (() => void) | undefined | null): Promise<T> {
+    if (this.canceled) {
+      this.canceled = false
+      return this
+    }
+    return super.finally(onfinally)
+  }
+}
