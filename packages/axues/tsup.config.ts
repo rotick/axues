b301b093828cd88cc8318b821a5c73d802177129
dongnaebi <zhouzixian@qq.com>
2023-02-03 00:56:27 +0800

    chore: fill package fields

--- packages/axues/tsup.config.ts
+++ packages/axues/tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   dts: true,
+  minify: true,
   external: ['vue', 'axios'],
-  format: ['cjs', 'esm', 'iife'],
+  format: ['cjs', 'esm'],
   splitting: false,
   sourcemap: true,
   clean: true
