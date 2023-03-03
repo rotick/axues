bc3d2d73f7fa1eace08b48856b053e37ca4eba70
dongnaebi <zhouzixian@qq.com>
2023-03-04 00:01:53 +0800

    docs(zh): determine the catalog

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -96,7 +96,24 @@ function sidebarGuide(lang?: string) {
       items: [
         { text: 'Basic usage', text_zh: '基础用法', link: '/guide/basic-usage' },
         { text: 'Request configuration', text_zh: '请求配置', link: '/guide/request-configuration' },
-        { text: 'Debounce', text_zh: '防抖', link: '/guide/debounce' }
+        { text: 'Request states and operation', text_zh: '请求状态及操作', link: '/guide/request-configuration' },
+        { text: 'Debounce', text_zh: '防抖', link: '/guide/debounce' },
+        { text: 'Pagination request', text_zh: '分页请求', link: '/guide/debounce' },
+        { text: 'Error retries', text_zh: '错误重试', link: '/guide/debounce' },
+        { text: 'Response caching', text_zh: '缓存请求结果', link: '/guide/debounce' },
+        { text: 'Interactive components', text_zh: '集成交互组件', link: '/guide/debounce' },
+        { text: 'Request callback', text_zh: '请求完成后的回调', link: '/guide/debounce' },
+        { text: 'Use outside of components', text_zh: '组件外使用', link: '/guide/debounce' }
+      ]
+    },
+    {
+      text: 'Global configurations',
+      text_zh: '全局配置',
+      collapsed: false,
+      items: [
+        { text: 'Request configuration', text_zh: '请求配置', link: '/guide/request-configuration' },
+        { text: 'Response / error handling', text_zh: '响应 / 错误处理', link: '/guide/request-configuration' },
+        { text: 'Rewrite default values', text_zh: '重写默认值', link: '/guide/debounce' }
       ]
     }
   ]
--- packages/docs/zh/guide/debounce.md
+++ packages/docs/zh/guide/debounce.md
@@ -87,7 +87,7 @@ const { data: suggest, action } = useAxues({
 
 ## 禁用防抖
 
-总有特殊的场景不需要任何的防抖机制，所以我们也得支持关掉防抖，只要给 `debounceMode` 赋值为 `none` 即可禁用防抖。
+总有特殊的场景不需要任何的防抖机制，只需要给 `debounceMode` 赋值为 `none` 即可禁用防抖。
 
 ```javascript
 import { useAxues } from 'axues'
